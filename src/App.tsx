b4454b28d8547fa0d8015a14501857fc6397189a
shahin <shahinahd3@gamil.com>
2024-03-10 16:19:36 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Routes >
+      <Route path="/" element={<h1>HI</h1>} />
+      <Route path="/new" element={<h1>new</h1>} />
+      <Route path="*" element={<Navigate to="/"/>} />
+    </Routes>
   )
 }
 
